63f6a572540e37402c8a43f3ebfc1fd5f0ee533a
Recep Darıcı <recepdarici4507@gmail.com>
2022-10-07 16:40:15 +0300

    1

---
+++ src/lambdaPractice/Methodlarim.java
@@ -0,0 +1,14 @@
+package lambdaPractice;
+
+public class Methodlarim {//seed (tohum) methodlar create edildi
+
+    public static void yazdir(Object obj) {
+
+    }
+
+
+    public static boolean ciftElemaniBul(int x){
+        return  x % 2==0;
+        // 2.yol return x % 2!=1;
+    }}
+
